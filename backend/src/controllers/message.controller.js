7da45e1378b16657667a29422f34f98768c8d475
manikanta rajesh <venkatrajeshnaidu@gmail.com>
2026-04-29 17:29:59 +0530

    feat: restrict sidebar to friends and populate requests

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -7,9 +7,9 @@ import { getReceiverSocketId, io } from "../lib/socket.js";
 export const getUsersForSidebar = async (req, res) => {
   try {
     const loggedInUserId = req.user._id;
-    const filteredUsers = await User.find({ _id: { $ne: loggedInUserId } }).select("-password");
+    const user = await User.findById(loggedInUserId).populate("friends", "-password");
 
-    res.status(200).json(filteredUsers);
+    res.status(200).json(user.friends);
   } catch (error) {
     console.error("Error in getUsersForSidebar: ", error.message);
     res.status(500).json({ message: "Internal server error" });
